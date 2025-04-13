32039899ee516f10477038ceaafdc7849e945e1c
Simon Carlsson <sicar@kth.se>
2025-04-13 18:52:15 +0200

    Merged functions

--- main.py
+++ main.py
@@ -1,54 +1,34 @@
 import json
 
-
-def extract_following(file_path):
+def extract_usernames(file_path, key=None):
     """
     Extract usernames from a JSON file.
 
     Args:
         file_path (str): Path to the JSON file.
-
-    Returns:
-        set: A set of usernames (str).
-    """
-
-    with open(file_path, "r") as file:
-        data = json.load(file)
-
-    usernames = set()
-
-    for entry in data.get("relationships_following", []):
-        entry_list = entry.get("string_list_data", [])
-        if entry_list:
-            usernames.add(entry_list[0].get("value"))
-    return usernames
-
-
-def extract_followers(file_path):
-    """
-    Extract usernames from a JSON file.
-
-    Args:
-        file_path (str): Path to the JSON file.
-
+        key (str, optional): JSON key for relationships. Default is None.
+    
     Returns:
         set: A set of usernames (str).
     """
     with open(file_path, "r") as file:
         data = json.load(file)
 
-    usernames = set()
+        if key:
+            data = data.get(key, [])
 
-    for entry in data:
-        entry_list = entry.get("string_list_data", [])
-        if entry_list:
-            usernames.add(entry_list[0].get("value"))
-    return usernames
-
+        usernames = set()
+        for entry in data:
+            entry_list = entry.get("string_list_data", [])
+            if entry_list and "value" in entry_list[0]:
+                usernames.add(entry_list[0]["value"])
+        return usernames
 
 if __name__ == "__main__":
-    following_usernames = extract_following("following.json")
-    followers_usernames = extract_followers("followers_1.json")
+    following_usernames = extract_usernames(
+        "following.json", key="relationships_following"
+    )
+    followers_usernames = extract_usernames("followers_1.json")
 
     not_following_back = following_usernames - followers_usernames
     you_dont_follow_back = followers_usernames - following_usernames
